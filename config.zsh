43165495717c1f19cc8f37cbdb9e03196dc6c9d2
t-taylor <thomas.taylor@infinity.co>
2022-01-12 09:13:04 +0000

    Mac check

--- config.zsh
+++ config.zsh
@@ -71,7 +71,13 @@ function mvr() {
 # z cd
 source $HOME/.config/zsh/zsh-z/zsh-z.plugin.zsh
 
-alias l='ls -lahG --color'
+if [ "$HOST" = "INF-FVFGN0ULQ05P" ]; then
+  alias l='ls -lahG'
+else
+  alias l='ls -lahG --color'
+fi
+
+
 alias atexmk="ls *.tex *.bib | entr -s 'latexmk --pdf -gg -f && latexmk -c'"
 alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
 alias push='git push'
